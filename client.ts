119c6426449380bff8fa8a51327f16188075489d
youtubeseosuite-afk <youtubeseosuite@gmail.com>
2026-09-08 08:03:40 +0200

    Delete client.ts

--- client.ts
+++
@@ -1,9 +0,0 @@
-// Path: lib/supabase/client.ts | Type: NEW
-import { createBrowserClient } from "@supabase/ssr";
-
-export function createClient() {
-  return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
-}
